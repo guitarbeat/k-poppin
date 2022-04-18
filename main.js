f8365a793921e00ae3fb8e7af0582d2280e7a36b
Glitch (nutritious-hip-balance) <none>
2022-04-18 23:22:00 +0000

    🌔🍣 Checkpoint ./main.js:43960396/20 ./bundle.css:43960396/28

--- main.js
+++ main.js
@@ -4082,7 +4082,7 @@ var app = (function() {
       maxAttempts: 6,
       startDate: "2022-04-011",
     },
-    Jt = ["Unlucky! At least you discovered a new song!", "You're a true girl group fan!", "!", "Amazing!", "Look at you go!", "Nicely done", "Almost got exposed!"];
+    Jt = ["Unlucky! At least you discovered a new song!", "You're a true girl group fan!", "Had to think twice?", "Amazing!", "Look at you go!", "Nicely done", "Almost got exposed!"];
 
   function Kt(t) {
     let n, r;
