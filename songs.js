1685c0720a62950688b87cd452ae046161e1dafd
Glitch (nutritious-hip-balance) <none>
2022-08-11 20:51:03 +0000

    😘📯 Checkpoint ./songs.js:190215/11228 ./index.html:190215/51 ./main.js:190215/21827 ./main.js copy:190215/275878

---
+++ songs.js
@@ -0,0 +1,318 @@
+let songs = [
+          {
+            url: "https://soundcloud.com/l2share95/red-velvet-psycho",
+            answer: "Red Velvet - Psycho",
+          },
+          {
+            url: "https://soundcloud.com/twice-57013/the-feels",
+            answer: "Twice - The Feels",
+          },
+          {
+            url: "https://soundcloud.com/xikanist/fromis_9-glass-shoes",
+            answer: "fromis_9 - Glass Shoes",
+          },
+          {
+            url: "https://soundcloud.com/destinee-nsombe/wjsn-cosmic-girls-i-wish",
+            answer: "WJSN - I Wish",
+          },
+          {
+            url: "https://soundcloud.com/seasaltfishes/red-velvet-rbb-really-bad",
+            answer: "Red Velvet - Really Bad Boy",
+          },
+          {
+            url: "https://soundcloud.com/kekkun89/t-ara-roly-poly",
+            answer: "T-ara - Roly Poly",
+          },
+          {
+            url: "https://soundcloud.com/wahyu_putra/sistar-touch-my-body-at-wahyoe",
+            answer: "Sistar - Touch My Body",
+          },
+          {
+            url: "https://soundcloud.com/itzy-music/wannabe",
+            answer: "ITZY - Wannabe",
+          },
+          {
+            url: "https://soundcloud.com/l2share47/loona-favorite",
+            answer: "Loona - favOriTe",
+          },
+          {
+            url: "https://soundcloud.com/not_ur_sepai/girls-generation-party",
+            answer: "Girl's Generation - Party",
+          },
+          {
+            url: "https://soundcloud.com/user-313773669/birthday-somi",
+            answer: "Somi - Birthday",
+          },
+          {
+            url: "https://soundcloud.com/xiz-thic/ioi-whatta-man-good-man",
+            answer: "I.O.I - Whatta Man",
+          },
+          {
+            url: "https://soundcloud.com/josedestroy/exid-updown",
+            answer: "EXID - Up&Down",
+          },
+          {
+            url: "https://soundcloud.com/rororomeo/fx-nu-nu-abo",
+            answer: "F(X) - Nu Abo",
+          },
+          {
+            url: "https://soundcloud.com/marshall_line/kill-bill",
+            answer: "Brown Eyed Girls - Kill Bill",
+          },
+          {
+            url: "https://soundcloud.com/twice-57013/fancy",
+            answer: "Twice - Fancy",
+          },
+          {
+            url: "https://soundcloud.com/l2share106/sunmi-pporappippam",
+            answer: "Sunmi - pporappippam",
+          },
+          {
+            url: "https://soundcloud.com/rodsepp/we-like",
+            answer: "Pristin - We Like",
+          },
+          {
+            url: "https://soundcloud.com/interscope/girls-generation-the-boys-8",
+            answer: "Girl's Generation - The Boys",
+          },
+          {
+            url: "https://soundcloud.com/l2share140/ive-eleven",
+            answer: "IVE - Eleven",
+          },
+          {
+            url: "https://soundcloud.com/itsvee/short-hair-aoa",
+            answer: "AOA - Short Hair",
+          },
+          {
+            url: "https://soundcloud.com/twice-57013/cheer-up",
+            answer: "Twice - Cheer Up",
+          },
+          {
+            url: "https://soundcloud.com/blackpinkofficial/how-you-like-that",
+            answer: "Blackpink - How You Like That",
+          },
+          {
+            url: "https://soundcloud.com/l2share131/fromis_9-we-go",
+            answer: "fromis_9 - We Go",
+          },
+          {
+            url: "https://soundcloud.com/mina-anh-967372179/very-very-very-ioi",
+            answer: "I.O.I - Very Very Very",
+          },
+          {
+            url: "https://soundcloud.com/user-913229388/f-x-4-walls",
+            answer: "F(X) - 4 Walls",
+          },
+          {
+            url: "https://soundcloud.com/sophia-jesa/hows-this-hyuna",
+            answer: "Hyuna - How's This?",
+          },
+          {
+            url: "https://soundcloud.com/twice-57013/more-more",
+            answer: "Twice - More and More",
+          },
+          {
+            url: "https://soundcloud.com/lisalady-yedek/stayc-stereotype",
+            answer: "StayC - Stereotype",
+          },
+          {
+            url: "https://soundcloud.com/dani-martins-neves/girls-generation-i-got-a-boy",
+            answer: "Girl's Generation - I Got A Boy",
+          },
+          {
+            url: "https://soundcloud.com/exo-baekhyun/miss-a-hush",
+            answer: "Miss A - Hush",
+          },
+          {
+            url: "https://soundcloud.com/user746128857/aoa-heart-attack",
+            answer: "AOA - Heart Attack",
+          },
+          {
+            url: "https://soundcloud.com/4minute/hot-issue",
+            answer: "4Minute - Hot Issue",
+          },
+          {
+            url: "https://soundcloud.com/boatbanana/red-velvet-dumb-dumb",
+            answer: "Red Velvet - Dumb Dumb",
+          },
+          {
+            url: "https://soundcloud.com/sophia-jesa/la-vie-en-rose-izone",
+            answer: "IZONE - La Vie En Rose",
+          },
+          {
+            url: "https://soundcloud.com/skkzyx/pristin-v-get-it",
+            answer: "Pristin V - Get It",
+          },
+          {
+            url: "https://soundcloud.com/emi-oneill/after-school-weeekly",
+            answer: "Weeekly - After School",
+          },
+          {
+            url: "https://soundcloud.com/aylin-leandro-ferre/weki-meki-i-dont-like-your-girlfriend",
+            answer: "Weki Meki - I Don't Like Your Girlfriend",
+          },
+          {
+            url: "https://soundcloud.com/twice-57013/tt",
+            answer: "Twice - TT",
+          },
+          {
+            url: "https://soundcloud.com/faby-queen/hyuna-red",
+            answer: "Hyuna - Red",
+          },
+          {
+            url: "https://soundcloud.com/eohu/abracadabra-brown-eyed-girls",
+            answer: "Brown Eyed Girls - Abracadabra",
+          },
+          {
+            url: "https://soundcloud.com/ernchern/like-a-cat-aoa",
+            answer: "AOA - Like A Cat",
+          },
+          {
+            url: "https://soundcloud.com/genrse/blackpink-whistle",
+            answer: "Blackpink - Whistle",
+          },
+          {
+            url: "https://soundcloud.com/somi_official/what-you-waiting-for",
+            answer: "Somi - What You Waiting For",
+          },
+          {
+            url: "https://soundcloud.com/l2share95/red-velvet-queendom",
+            answer: "Red Velvet - Queendom",
+          },
+          {
+            url: "https://soundcloud.com/l2share77/loona-butterfly",
+            answer: "Loona - Butterfly",
+          },
+          {
+            url: "https://soundcloud.com/chic-el/wonder-girls-nobody-english-version",
+            answer: "Wonder Girls - Nobody",
+          },
+          {
+            url: "https://soundcloud.com/greenmusic-channel/bo-peep-bo-peep-t-ara",
+            answer: "T-ara - Bo Peep Bo Peep",
+          },
+          {
+            url: "https://soundcloud.com/l2share127/stayc-asap",
+            answer: "StayC - ASAP",
+          },
+          {
+            url: "https://soundcloud.com/seoyeon-962740403/oh-my-girl-nonstop-2",
+            answer: "Oh My Girl - Nonstop",
+          },
+          {
+            url: "https://soundcloud.com/tm-950004586/orange-caramel-catallena",
+            answer: "Orange Caramel - Catallena",
+          },
+          {
+            url: "https://soundcloud.com/faiz-zarul/dia-will-you-go-out-with-me-1",
+            answer: "DIA - Will You Go Out With Me",
+          },
+          {
+            url: "https://soundcloud.com/twice-57013/alcohol-free",
+            answer: "Twice - Alcohol Free",
+          },
+          {
+            url: "https://soundcloud.com/seasaltfishes/power-up",
+            answer: "Red Velvet - Power Up",
+          },
+          {
+            url: "https://soundcloud.com/l2share59/exid-lady",
+            answer: "EXID - Lady",
+          },
+          {
+            url: "https://soundcloud.com/mungikim/pristin-wee-woo",
+            answer: "Pristin - Wee Woo",
+          },
+          {
+            url: "https://soundcloud.com/l2share138/kep1er-wa-da-da",
+            answer: "Kep1er - Wa Da Da",
+          },
+          {
+            url: "https://soundcloud.com/user-820261227/aespa-black-mamba-1",
+            answer: "Aespa - Black Mamba",
+          },
+          {
+            url: "https://soundcloud.com/crystal7t/crazy-4minute",
+            answer: "4Minute - Crazy",
+          },
+          {
+            url: "https://soundcloud.com/l2share44/fromis_9-love-bomb",
+            answer: "fromis_9 - Love Bomb",
+          },
+          {
+            url: "https://soundcloud.com/kusumahusnina/f-x-hot-summer",
+            answer: "F(X) - Hot Summer",
+          },
+          {
+            url: "https://soundcloud.com/9flawless/blackpink-as-if-its-your-last",
+            answer: "Blackpink - As If It's Your Last",
+          },
+          {
+            url: "https://soundcloud.com/handa-vempire-chanjuan/wonder-girls-tell-me-2012-korean-version",
+            answer: "Wonder Girls - Tell Me",
+          },
+          {
+            url: "https://soundcloud.com/l2share47/weki-meki-crush",
+            answer: "Weki Meki - Crush",
+          },
+          {
+            url: "https://soundcloud.com/mcnlight/hyuna-babe",
+            answer: "Hyuna - Babe",
+          },
+          {
+            url: "https://soundcloud.com/l2share49/red-velvet-zimzalabim",
+            answer: "Red Velvet - Zimzalabim",
+          },
+          {
+            url: "https://soundcloud.com/twice-57013/i-cant-stop-me",
+            answer: "Twice - I Can't Stop Me",
+          },
+          {
+            url: "https://soundcloud.com/olivier-baudhuin/t-ara-lovey-dovey",
+            answer: "T-ara - Lovey Dovey",
+          },
+          {
+            url: "https://soundcloud.com/hoxl1e5ylvbp/miss-a-bad-girl-good-girl",
+            answer: "Miss A - Bad Girl Good Girl",
+          },
+          {
+            url: "https://soundcloud.com/l2share52/izone-violeta",
+            answer: "IZONE - Violeta",
+          },
+          {
+            url: "https://soundcloud.com/ph-ng-vy-35477112/im-so-sick",
+            answer: "APink - I'm So Sick",
+          },
+          {
+            url: "https://soundcloud.com/quan-mai-anh/lovelyz-obliviate",
+            answer: "LovelyZ - Obliviate",
+          },
+          {
+            url: "https://soundcloud.com/shwi_wie/cherry-bullet-love-so-sweet",
+            answer: "Cherry Bullet - Love So Sweet",
+          },
+          {
+            url: "https://soundcloud.com/l2share128/taeyeon-weekend",
+            answer: "Taeyeon - Weekend",
+          },
+          {
+            url: "https://soundcloud.com/karina-irgintya/gfriend-glass-bead",
+            answer: "GFriend - Glass Bead",
+          },
+          {
+            url: "https://soundcloud.com/wfxb0pd3jqyx/brave-girls-rollin",
+            answer: "Brave Girls - Rollin'",
+          },
+          {
+            url: "https://soundcloud.com/ohmygirl-music/coloring-book",
+            answer: "Oh My Girl - Coloring Book",
+          },
+          {
+            url: "https://soundcloud.com/l2share86/fanatics-sunday",
+            answer: "Fanatics - Sunday",
+          },
+          {
+            url: "https://soundcloud.com/2ne1official/i-am-the-best",
+            answer: "2NE1 - I Am The Best",
+          },
+        ]
